c1d464be4594378e615269409a2bd7e191aa2b77
AnthonyCointre <8732@holbertonstudents.com>
2024-03-25 16:12:38 +0100

    creation main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _printf - a
+ * @format: a
+ *
+ * Return: a
+*/
+
+int _printf(const char *format, ...)
+{
+	va_list ptr;
+
+	va_start(ptr, format);
+
+
+}
+
+/**
+ * main - a
+ *
+ * Return: a
+*/
+
+int main(void)
+{
+	return (0);
+}
