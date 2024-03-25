a21f5340e2488e22707bdefc5d0a6ae53ef3c4cf
AnthonyCointre <8732@holbertonstudents.com>
2024-03-25 15:14:13 +0100

    creation main.h

--- main.h
+++ main.h
@@ -1,6 +1,25 @@
 #ifndef PRINTF_H
 #define PRINTF_H
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * struct printer - a
+ * @symbol: a
+ * @print: a
+ */
+
+typedef struct printf
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+} printf_t;
+
+void print_char(va_list arg);
+void print_string(va_list arg);
 int _printf(const char *format, ...);
 int _putchar(char c);
 
