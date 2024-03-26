cbc4a7f9a7e3da966d107a929572bf9bf7e68e57
AnthonyCointre <8732@holbertonstudents.com>
2024-03-26 10:23:18 +0100

    creation test

--- function_s.c
+++ function_s.c
@@ -2,12 +2,27 @@
 
 /**
  * print_string - a
- * @va_list: a
+ * @arg: a
  *
  * Return: a
 */
 
-int print_string(va_list)
+int print_string(va_list arg)
 {
+	int i;
+	char *string;
 
+	string = va_arg(arg, char *);
+
+	if (string == NULL)
+	{
+		string = "(null)";
+	}
+
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		_write(string[i]);
+	}
+
+	return (i);
 }
