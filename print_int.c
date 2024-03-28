f1c9caad43fb8102882ced0041c0004d714a2d4d
gofastpanam <8972@holbertonstudents.com>
2024-03-28 14:39:02 +0100

    modif len in function print_int

--- print_int.c
+++ print_int.c
@@ -1,14 +1,27 @@
 #include "main.h"
 
 /**
+* print_int - a
+* @c: a
 * print_int - Prints an integer to the standard output.
 * @c: The integer to be printed.
 *
+* Return: a
 * Return: void
 */
 
 int print_int(int c)
 {
+	int len = 0;
+
+	if (c < 0)
+	{
+	_putchar('-');
+	c = -c;
+	}
+
 	_putchar(c);
-	return (1);
+	len++;
+
+	return (len);
 }
